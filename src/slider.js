8a67a175d3009945ba139fed458aaae3800c23a8
Szymon Kummer <szykummer@gmail.com>
2018-05-14 13:22:25 +0200

    header update

--- src/slider.js
+++ src/slider.js
@@ -10,15 +10,4 @@ $(function() {
 
   }
    itemrInterval = setInterval(itemNext, 4000);
-
-  $('.prev').on('click', function() {
-    inWrap.animate({left: '0%'}, 1000, function() {
-      inWrap.css('left', '-100%');
-      $('.item').first().before($('.item').last());
-    });
-  });
-
-  $('.next').on('click', function() {
-    itemNext();
-  });
 });
